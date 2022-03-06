30cdffd5a0c3b953229d0a510490b82ee4e9328f
Marcin <marcin.obiedz@gmail.com>
2022-03-06 14:09:46 +0100

    blocking button

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("test")
+const btn = document.querySelector('#my-button');
+
+if (btn) {
+    btn.addEventListener('click', () => {
+        let myDate;
+        for (let i = 0; i < 10000000; i++) {
+            let date = new Date();
+            myDate = date;
+        }
+
+        console.log(myDate);
+
+        let pElem = document.createElement('p');
+        pElem.textContent = 'This is a newly-added paragraph.';
+        document.body.appendChild(pElem);
+    });
+}
