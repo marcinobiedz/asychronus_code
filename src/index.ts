0b7d0675fd1bb83f2afeb6bd81cae09b9c43c574
Marcin <marcin.obiedz@gmail.com>
2022-03-07 13:33:38 +0100

    circles example

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,37 @@
-const btn = document.querySelector('#my-button');
+const canvas = document.createElement('canvas');
+canvas.width = 640;
+canvas.height = 480;
+document.body.appendChild(canvas);
+let ctx = canvas.getContext('2d');
 
-if (btn) {
-    btn.addEventListener('click', () => {
-        let myDate;
-        for (let i = 0; i < 10000000; i++) {
-            let date = new Date();
-            myDate = date;
+let alertBtn = document.querySelector('.alert');
+let fillBtn = document.querySelector('.fill');
+
+function degToRad(degrees: number) {
+    return degrees * Math.PI / 180;
+}
+
+function random(min: number, max: number) {
+    var num = Math.floor(Math.random() * (max - min)) + min;
+    return num;
+}
+
+function expensiveOperation() {
+    if(ctx){
+        for (let i = 0; i < 1000000; i++) {
+            ctx.fillStyle = 'rgba(0,0,255, 0.2)';
+            ctx.beginPath();
+            ctx.arc(random(0, canvas.width), random(0, canvas.height), 10, degToRad(0), degToRad(360), false);
+            ctx.fill()
         }
+    }
+}
 
-        console.log(myDate);
-
-        let pElem = document.createElement('p');
-        pElem.textContent = 'This is a newly-added paragraph.';
-        document.body.appendChild(pElem);
-    });
+if(fillBtn && alertBtn){
+    
+    fillBtn.addEventListener('click', expensiveOperation);
+    
+    alertBtn.addEventListener('click', () =>
+        alert('You clicked me!')
+    );
 }
