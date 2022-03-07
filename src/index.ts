8c16786917ac81d0ab724fce97a7db3c973e3172
Marcin <marcin.obiedz@gmail.com>
2022-03-07 13:44:02 +0100

    worker example

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,16 @@
-const canvas = document.createElement('canvas');
-canvas.width = 640;
-canvas.height = 480;
-document.body.appendChild(canvas);
-let ctx = canvas.getContext('2d');
+const btn = document.querySelector('button');
+const worker = new Worker('worker.js');
 
-let alertBtn = document.querySelector('.alert');
-let fillBtn = document.querySelector('.fill');
+if (btn) {
+    btn.addEventListener('click', () => {
+        worker.postMessage('Go!');
 
-function degToRad(degrees: number) {
-    return degrees * Math.PI / 180;
-}
+        let pElem = document.createElement('p');
+        pElem.textContent = 'This is a newly-added paragraph.';
+        document.body.appendChild(pElem);
+    });
 
-function random(min: number, max: number) {
-    var num = Math.floor(Math.random() * (max - min)) + min;
-    return num;
-}
-
-function expensiveOperation() {
-    if(ctx){
-        for (let i = 0; i < 1000000; i++) {
-            ctx.fillStyle = 'rgba(0,0,255, 0.2)';
-            ctx.beginPath();
-            ctx.arc(random(0, canvas.width), random(0, canvas.height), 10, degToRad(0), degToRad(360), false);
-            ctx.fill()
-        }
+    worker.onmessage = function (e) {
+        console.log(e.data);
     }
-}
-
-if(fillBtn && alertBtn){
-    
-    fillBtn.addEventListener('click', expensiveOperation);
-    
-    alertBtn.addEventListener('click', () =>
-        alert('You clicked me!')
-    );
 }
