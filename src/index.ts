5e90541de670f571585406dce2d111755562e57a
Marcin <marcin.obiedz@gmail.com>
2022-03-07 14:03:38 +0100

    xhr coffee example

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-const fetched = fetch('http://example.com/movies.json')
-fetched.json()
+function loadAsset(url: string, type: XMLHttpRequestResponseType, callback: (arg: any) => void) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = type;
+
+    xhr.onload = function () {
+        callback(xhr.response);
+    };
+
+    xhr.send();
+}
+
+function displayImage(blob: Blob) {
+    let objectURL = URL.createObjectURL(blob);
+
+    let image = document.createElement('img');
+    image.src = objectURL;
+    document.body.appendChild(image);
+}
+
+loadAsset('coffee.jpg', 'blob', displayImage);
