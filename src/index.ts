d90959a4f1f5de6be62466a93dc5fbdecd1e5e33
Marcin <marcin.obiedz@gmail.com>
2022-03-07 15:50:56 +0100

    custom promise reject

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-let timeoutPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('Success!');
-    }, 2000);
-});
+function timeoutPromise(message: string, interval: number) {
+    return new Promise((resolve, reject) => {
+        if (message === '' || typeof message !== 'string') {
+            reject('Message is empty or not a string');
+        } else if (interval < 0 || typeof interval !== 'number') {
+            reject('Interval is negative or not a number');
+        } else {
+            setTimeout(() => {
+                resolve(message);
+            }, interval);
+        }
+    });
+}
 
-timeoutPromise
+timeoutPromise({} as any, 1000)
     .then(message => {
         alert(message);
     })
+    .catch(e => {
+        console.log('Error: ' + e);
+    });
