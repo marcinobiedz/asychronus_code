854fd57f2699e6bb3ddfd3cc4c2a500f29c36dbe
Marcin <marcin.obiedz@gmail.com>
2022-03-07 13:50:06 +0100

    not working fetch

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,2 @@
-const btn = document.querySelector('button');
-const worker = new Worker('worker.js');
-
-if (btn) {
-    btn.addEventListener('click', () => {
-        worker.postMessage('Go!');
-
-        let pElem = document.createElement('p');
-        pElem.textContent = 'This is a newly-added paragraph.';
-        document.body.appendChild(pElem);
-    });
-
-    worker.onmessage = function (e) {
-        console.log(e.data);
-    }
-}
+const fetched = fetch('http://example.com/movies.json')
+fetched.json()
