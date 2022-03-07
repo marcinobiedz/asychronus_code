2e14be26c1fda0354e51bd35c08022ff23f34d2d
Marcin <marcin.obiedz@gmail.com>
2022-03-07 16:28:06 +0100

    async await coffee

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
-function timeoutPromise(message: string, interval: number) {
-    return new Promise((resolve, reject) => {
-        if (message === '' || typeof message !== 'string') {
-            reject('Message is empty or not a string');
-        } else if (interval < 0 || typeof interval !== 'number') {
-            reject('Interval is negative or not a number');
-        } else {
-            setTimeout(() => {
-                resolve(message);
-            }, interval);
-        }
-    });
-}
+// Call the fetch() method to fetch the image, and store it in a variable
+async function myFetch() {
+    let response = await fetch('coffee.jpg');
+    // Use a then() block to respond to the promise's successful completion
+    // by taking the returned response and running blob() on it to transform it into a blob
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      return await response.blob();
+    }
+    // blob() also returns a promise; when it successfully completes it returns
+    // The blob object in the callback
+  }
 
-timeoutPromise({} as any, 1000)
-    .then(message => {
-        alert(message);
-    })
-    .catch(e => {
-        console.log('Error: ' + e);
-    });
+  myFetch().then((blob) => {
+    // Create an object URL that points to the blob object
+    let objectURL = URL.createObjectURL(blob);
+    // Create an <img> element to display the blob (it's an image)
+    let image = document.createElement('img');
+    // Set the src of the <img> to the object URL so the image displays it
+    image.src = objectURL;
+    // Append the <img> element to the DOM
+    document.body.appendChild(image);
+  }).catch(e => console.log(e));
